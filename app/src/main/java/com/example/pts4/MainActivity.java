337f1ef9c00df313cf515877555713594a62a417
ToRr0aN <unetou.72@hotmail.fr>
2021-01-22 16:28:54 +0100

    test git dans le salon git

--- app/src/main/java/com/example/pts4/MainActivity.java
+++ app/src/main/java/com/example/pts4/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //vérif gits discord
+        //test git dans le salon git
     }
 }
