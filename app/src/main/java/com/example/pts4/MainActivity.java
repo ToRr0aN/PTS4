cda5d3075b7509a380411a1adcae52643034f127
ToRr0aN <unetou.72@hotmail.fr>
2021-01-22 16:25:07 +0100

    vérif gits discord

--- app/src/main/java/com/example/pts4/MainActivity.java
+++ app/src/main/java/com/example/pts4/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test gits discord 2
+        //vérif gits discord
     }
 }
